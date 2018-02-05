f836e5ac5ca902cfe3790ec1c45cf5ede847d9aa
renyaoxiang <97239080@qq.com>
2018-02-05 11:39:55 +0800

    su

---
+++ src/pages/resultsView/mutation-custom/MutationMapper.tsx
@@ -0,0 +1,207 @@
+import * as React from "react";
+import { observer } from "mobx-react";
+import { Button, ButtonGroup } from "react-bootstrap";
+import LoadingIndicator from "../../../shared/components/loadingIndicator/LoadingIndicator";
+import StructureViewerPanel from "../../../shared/components/structureViewer-custom/StructureViewerPanel";
+import DiscreteCNACache from "../../../shared/cache/DiscreteCNACache";
+import GenomeNexusEnrichmentCache from "../../../shared/cache/GenomeNexusEnrichment";
+import OncoKbEvidenceCache from "../../../shared/cache/OncoKbEvidenceCache";
+import PubMedCache from "../../../shared/cache/PubMedCache";
+import CancerTypeCache from "../../../shared/cache/CancerTypeCache";
+import MutationCountCache from "../../../shared/cache/MutationCountCache";
+import { IMyCancerGenomeData } from "../../../shared/model/MyCancerGenome";
+import PdbHeaderCache from "../../../shared/cache/PdbHeaderCache";
+import { DEFAULT_PROTEIN_IMPACT_TYPE_COLORS } from "../../../shared/lib/MutationUtils";
+import { MutationMapperStore } from "./MutationMapperStore";
+import ResultsViewMutationTable from "./ResultsViewMutationTable";
+import LollipopMutationPlot from "../../../shared/components/lollipopMutationPlot-custom/LollipopMutationPlot";
+import ProteinImpactTypePanel from "../../../shared/components/mutationTypePanel/ProteinImpactTypePanel";
+import ProteinChainPanel from "../../../shared/components/proteinChainPanel-custom/ProteinChainPanel";
+import { computed, action, observable } from "mobx";
+import MutationRateSummary from "./MutationRateSummary";
+
+// Anything from App config will be included in mutation mapper config
+export interface IMutationMapperConfig {
+	userEmailAddress?: string;
+	showCivic?: boolean;
+	showHotspot?: boolean;
+	showMyCancerGenome?: boolean;
+	showOncoKB?: boolean;
+	showGenomeNexus?: boolean;
+	isoformOverrideSource?: string;
+}
+
+export interface IMutationMapperProps {
+	store: MutationMapperStore;
+	config: IMutationMapperConfig;
+	studyId?: string;
+	myCancerGenomeData?: IMyCancerGenomeData;
+	discreteCNACache?: DiscreteCNACache;
+	genomeNexusEnrichmentCache?: GenomeNexusEnrichmentCache;
+	oncoKbEvidenceCache?: OncoKbEvidenceCache;
+	cancerTypeCache?: CancerTypeCache;
+	mutationCountCache?: MutationCountCache;
+	pdbHeaderCache?: PdbHeaderCache;
+	pubMedCache?: PubMedCache;
+}
+
+@observer
+export default class MutationMapper extends React.Component<IMutationMapperProps, {}> {
+	@observable protected is3dPanelOpen = false;
+	@observable lollipopPlotGeneX: number = 0;
+	@observable geneWidth: number = 665;
+
+	private handlers: any;
+
+	constructor(props: IMutationMapperProps) {
+		super(props);
+
+		this.open3dPanel = this.open3dPanel.bind(this);
+		this.close3dPanel = this.close3dPanel.bind(this);
+		this.toggle3dPanel = this.toggle3dPanel.bind(this);
+		this.handlers = {
+			resetDataStore: () => {
+				this.props.store.dataStore.resetFilterAndSelection();
+			},
+			onXAxisOffset: action((offset: number) => {
+				this.lollipopPlotGeneX = offset;
+			})
+		};
+	}
+
+	@computed
+	get geneSummary(): JSX.Element {
+		const hugoGeneSymbol = this.props.store.gene.hugoGeneSymbol;
+		const uniprotId = this.props.store.uniprotId.result;
+		const transcriptId =
+			this.props.store.canonicalTranscript.result && this.props.store.canonicalTranscript.result.transcriptId;
+
+		return (
+			<div style={{ paddingBottom: 10 }}>
+				<h4>{hugoGeneSymbol}</h4>
+				<div className={this.props.store.uniprotId.result ? "" : "invisible"}>
+					<span>UniProt: </span>
+					<a href={`http://www.uniprot.org/uniprot/${uniprotId}`} target="_blank">
+						{uniprotId}
+					</a>
+				</div>
+				<div className={this.props.store.canonicalTranscript.result ? "" : "invisible"}>
+					<span>Transcript: </span>
+					<a
+						href={`http://grch37.ensembl.org/homo_sapiens/Transcript/Summary?t=${transcriptId}`}
+						target="_blank"
+					>
+						{transcriptId}
+					</a>
+				</div>
+			</div>
+		);
+	}
+
+	@computed
+	get mutationRateSummary(): JSX.Element | null {
+		// TODO we should not be even calculating mskImpactGermlineConsentedPatientIds for studies other than msk impact
+		if (
+			this.props.store.germlineConsentedSamples.result &&
+			this.props.store.mutationData.isComplete &&
+			this.props.store.mutationData.result.length > 0
+		) {
+			return (
+				<MutationRateSummary
+					hugoGeneSymbol={this.props.store.gene.hugoGeneSymbol}
+					molecularProfileIdToMolecularProfile={this.props.store.molecularProfileIdToMolecularProfile}
+					mutations={this.props.store.mutationData.result}
+					samples={this.props.store.samples.result!}
+					germlineConsentedSamples={this.props.store.germlineConsentedSamples}
+				/>
+			);
+		} else {
+			return null;
+		}
+	}
+
+	public render() {
+		return (
+			<div>
+				{this.is3dPanelOpen && (
+					<StructureViewerPanel
+						mutationDataStore={this.props.store.dataStore}
+						pdbChainDataStore={this.props.store.pdbChainDataStore}
+						pdbAlignmentIndex={this.props.store.indexedAlignmentData}
+						pdbHeaderCache={this.props.pdbHeaderCache}
+						residueMappingCache={this.props.store.residueMappingCache}
+						uniprotId={this.props.store.uniprotId.result}
+						onClose={this.close3dPanel}
+						{...DEFAULT_PROTEIN_IMPACT_TYPE_COLORS}
+					/>
+				)}
+
+				<LoadingIndicator isLoading={this.props.store.mutationData.isPending} />
+				{!this.props.store.mutationData.isPending && (
+					<div>
+						<LoadingIndicator isLoading={this.props.store.pfamDomainData.isPending} />
+						{!this.props.store.pfamDomainData.isPending && (
+							<div style={{ display: "flex" }}>
+								<div className="borderedChart" style={{ marginRight: 10 }}>
+									<LollipopMutationPlot
+										store={this.props.store}
+										onXAxisOffset={this.handlers.onXAxisOffset}
+										geneWidth={this.geneWidth}
+										{...DEFAULT_PROTEIN_IMPACT_TYPE_COLORS}
+									/>
+									<ProteinChainPanel
+										store={this.props.store}
+										pdbHeaderCache={this.props.pdbHeaderCache}
+										geneWidth={this.geneWidth}
+										geneXOffset={this.lollipopPlotGeneX}
+										maxChainsHeight={200}
+									/>
+								</div>
+
+								<div className="mutationMapperMetaColumn">
+									{this.geneSummary}
+
+									{this.mutationRateSummary}
+
+									<div>
+										<ProteinImpactTypePanel
+											dataStore={this.props.store.dataStore}
+											{...DEFAULT_PROTEIN_IMPACT_TYPE_COLORS}
+										/>
+									</div>
+
+									<button
+										className="btn btn-default btn-sm"
+										disabled={this.props.store.pdbChainDataStore.allData.length === 0}
+										onClick={this.toggle3dPanel}
+									>
+										View 3D Structure
+									</button>
+								</div>
+							</div>
+						)}
+						<hr style={{ marginTop: 20 }} />
+					</div>
+				)}
+			</div>
+		);
+	}
+
+	private toggle3dPanel() {
+		if (this.is3dPanelOpen) {
+			this.close3dPanel();
+		} else {
+			this.open3dPanel();
+		}
+	}
+
+	private open3dPanel() {
+		this.is3dPanelOpen = true;
+		this.props.store.pdbChainDataStore.selectFirstChain();
+	}
+
+	private close3dPanel() {
+		this.is3dPanelOpen = false;
+		this.props.store.pdbChainDataStore.selectUid();
+	}
+}
---
+++ src/pages/resultsView/mutation-custom/Mutations.tsx
@@ -0,0 +1,128 @@
+import * as React from "react";
+import { observer } from "mobx-react";
+import { MSKTabs, MSKTab } from "../../../shared/components/MSKTabs/MSKTabs";
+import { ResultsViewPageStore } from "../ResultsViewPageStore";
+import MutationMapper from "./MutationMapper";
+import { observable, computed } from "mobx";
+import { AppConfig } from "../../../config/appConfig";
+import "./mutations.scss";
+import { filterCBioPortalWebServiceData } from "../../../shared/lib/oql/oqlfilter";
+import accessors from "../../../shared/lib/oql/accessors";
+import Loader from "../../../shared/components/loadingIndicator/LoadingIndicator";
+import { MutationMapperStore } from "./MutationMapperStore";
+
+export interface IMutationsPageProps {
+	routing?: any;
+	store: ResultsViewPageStore;
+}
+
+@observer
+export default class Mutations extends React.Component<IMutationsPageProps, {}> {
+	@observable mutationsGeneTab: string;
+
+	constructor(props: IMutationsPageProps) {
+		super(props);
+		this.handleTabChange.bind(this);
+		this.mutationsGeneTab = this.props.store.hugoGeneSymbols![0];
+	}
+
+	public render() {
+		// use routing if available, if not fall back to the observable variable
+		const activeTabId = this.props.routing
+			? this.props.routing.location.query.mutationsGeneTab
+			: this.mutationsGeneTab;
+
+		return (
+			<div>
+				<Loader isLoading={this.props.store.mutationMapperStores.isPending} />
+				{this.props.store.mutationMapperStores.isComplete && (
+					<MSKTabs
+						id="mutationsPageTabs"
+						activeTabId={activeTabId}
+						onTabClick={(id: string) => this.handleTabChange(id)}
+						className="secondaryTabs resultsPageMutationsGeneTabs"
+						enablePagination={true}
+						arrowStyle={{ "line-height": 0.8 }}
+						tabButtonStyle="pills"
+						unmountOnHide={true}
+					>
+						{this.generateTabs(this.props.store.hugoGeneSymbols!)}
+					</MSKTabs>
+				)}
+			</div>
+		);
+	}
+
+	protected getMutationMapperStore(geneName: string) {
+		const {
+			config,
+			gene,
+			samples,
+			mutations,
+			getMutationDataCache,
+			genomeNexusEnrichmentCache,
+			getMutationCountCache,
+			molecularProfileIdToMolecularProfile,
+			germlineConsentedSamples
+		} = {
+			config: AppConfig,
+			gene: null,
+			samples: null,
+			mutations: null,
+			getMutationDataCache: null,
+			genomeNexusEnrichmentCache: null,
+			getMutationCountCache: null,
+			molecularProfileIdToMolecularProfile: null,
+			germlineConsentedSamples: null
+		};
+		return new MutationMapperStore(
+			config,
+			gene,
+			samples,
+			mutations,
+			getMutationDataCache,
+			genomeNexusEnrichmentCache,
+			getMutationCountCache,
+			molecularProfileIdToMolecularProfile,
+			germlineConsentedSamples
+		);
+	}
+
+	protected generateTabs(genes: string[]) {
+		const tabs: JSX.Element[] = [];
+
+		genes.forEach((gene: string) => {
+			const mutationMapperStore: MutationMapperStore = this.getMutationMapperStore(gene);
+			if (mutationMapperStore) {
+				tabs.push(
+					<MSKTab key={gene} id={gene} linkText={gene}>
+						<MutationMapper
+							store={mutationMapperStore}
+							discreteCNACache={this.props.store.discreteCNACache}
+							genomeNexusEnrichmentCache={this.props.store.genomeNexusEnrichmentCache}
+							oncoKbEvidenceCache={this.props.store.oncoKbEvidenceCache}
+							pubMedCache={this.props.store.pubMedCache}
+							cancerTypeCache={this.props.store.cancerTypeCache}
+							mutationCountCache={this.props.store.mutationCountCache}
+							pdbHeaderCache={this.props.store.pdbHeaderCache}
+							myCancerGenomeData={this.props.store.myCancerGenomeData}
+							config={AppConfig as any}
+						/>
+					</MSKTab>
+				);
+			}
+		});
+
+		return tabs;
+	}
+
+	protected handleTabChange(id: string) {
+		// update the hash if routing exits
+		if (this.props.routing) {
+			this.props.routing.updateRoute({ mutationsGeneTab: id });
+		} else {
+			// update the observable if no routing
+			this.mutationsGeneTab = id;
+		}
+	}
+}
